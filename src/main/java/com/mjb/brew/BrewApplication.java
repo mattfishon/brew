58b3bd0d6c08f0e7170838112b9a53cfab038405
tango <tango@10.0.0.60>
2018-05-12 11:47:00 -0600

    Init

---
+++ src/main/java/com/mjb/brew/BrewApplication.java
@@ -0,0 +1,12 @@
+package com.mjb.brew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mjb/brew/BrewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mjb.brew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
